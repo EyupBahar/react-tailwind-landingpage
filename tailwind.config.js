da1eea7788c14f1b5bdeef57fae19caad436be20
Eyup BAHAR <75902322+C7204-OMER@users.noreply.github.com>
2022-03-02 13:13:20 +0300

    add updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 module.exports = {
   mode: 'jit',
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', './public/index.html'
+  ],
+    darkMode: 'media',
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: theme => ({
+        'brand-color' : '#4c3398',
+        'primary-brand-color' : '#5d3ebc',
+        'secondary-brand-color' : '#7849f7',
+      })
+    },
+      gradientColorStops: theme => ({
+        'brand-color' : '#4c3398',
+        'primary-brand-color' : '#5d3ebc',
+        'secondary-brand-color' : '#7849f7',
+    })
   },
   plugins: [],
 }
